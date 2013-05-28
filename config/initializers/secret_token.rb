5d0962b25b278e3a613ca7f0e0a738cbd50c3dd0
James Smith <James@gaslight.co>
2013-05-28 15:29:05 -0400

    initial ember + rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anthracite2::Application.config.secret_key_base = '92c4f7052681a58504dbf68e366927ef5a966df6e03e5fedf005f1cf97cb7faf8db771fea0b29896f366ac064bdd19b2ef0c7b77225b54cddb3bb3ef47a76e75'
